0ec42e6112316e87c19ad0574d72eb9544baeb5d
Nymul Islam <nymulislam.dev@gmail.com>
2025-09-17 21:31:25 +0600

    category migration and model updated

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,4 +9,12 @@ class Category extends Model
 {
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
     use HasFactory;
+
+    protected $table = 'categories';
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'is_active'
+    ];
 }
